9051c1da83e29a7a8baaaebf685cf8043cbd4e72
Luke Ramljak <lukeramljak@proton.me>
2023-11-20 22:10:38 +0800

    add remove button to card

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const library = [];
-const form = document.getElementById("form");
 
 function Book(title, author, pages, read) {
   (this.title = title),
@@ -8,7 +7,7 @@ function Book(title, author, pages, read) {
     (this.read = read);
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(form) {
   const book = new Book(
     form.title.value,
     form.author.value,
@@ -18,9 +17,37 @@ function addBookToLibrary() {
   library.push(book);
 }
 
-form.addEventListener("submit", (event) => {
+function addBookToGrid() {
+  const latestIndex = library.length - 1;
+  const card = document.createElement('div');
+  const button = document.createElement('button');
+  const grid = document.getElementById('bookGrid');
+
+  card.className = 'card';
+  card.setAttribute('data-index', latestIndex);
+  card.innerText = library[latestIndex].title;
+
+  button.className = 'btn-remove';
+  button.setAttribute('data-index', latestIndex);
+  button.innerText = 'Remove';
+
+  card.appendChild(button);
+  grid.appendChild(card);
+
+  button.addEventListener('click', () => {
+    const index = button.getAttribute('data-index');
+    removeBook(index);
+    grid.removeChild(card);
+  });
+}
+
+function removeBook(index) {
+  library.splice(index, 1);
+}
+
+document.getElementById('form').addEventListener('submit', (event) => {
   event.preventDefault();
-  addBookToLibrary();
-  displayBooks();
-  form.reset();
+  addBookToLibrary(event.currentTarget);
+  addBookToGrid();
+  event.currentTarget.reset();
 });
