8b0253b481ce92084544f662b5ebef40a899fd13
Luke Ramljak <lukeramljak@proton.me>
2023-11-22 22:28:58 +0800

    improve list updating

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const library = [
   {
-    title: 'Book1',
-    author: 'Author',
-    pages: 100,
+    title: 'Lord of the Rings',
+    author: 'J.R.R Tolkien',
+    pages: 580,
     read: 'Yes',
   },
   {
-    title: 'Book2',
-    author: 'Author',
-    pages: 100,
+    title: 'American Psycho',
+    author: 'Some Woman',
+    pages: 350,
     read: 'No',
   },
 ];
@@ -20,7 +20,7 @@ function Book(title, author, pages, read) {
     (this.read = read);
 }
 
-function addBookToLibrary(form) {
+const addBookToLibrary = (form) => {
   const book = new Book(
     form.title.value,
     form.author.value,
@@ -28,58 +28,50 @@ function addBookToLibrary(form) {
     form.read.value
   );
   library.push(book);
-}
+};
 
-function createBookCard(book, index) {
+const createCard = (book, index) => {
+  const grid = document.getElementById('bookGrid');
   const card = document.createElement('div');
+
   card.className = 'card';
   card.setAttribute('data-index', index);
-  card.innerHTML = `
-  Title: ${book.title} 
-  <br>
-  Author: ${book.author}
-  <br>
-  Pages: ${book.pages}
-  <br>
-  Read: ${book.read}
-  `;
-  return card;
-}
 
-function createRemoveButton(index, card, grid) {
+  card.textContent = `${book.title} by ${book.author}, ${book.pages} pages.`;
+
   const button = document.createElement('button');
   button.className = 'btn-remove';
   button.setAttribute('data-index', index);
-  button.innerText = 'Remove';
+  button.textContent = 'Remove';
+  card.appendChild(button);
 
   button.addEventListener('click', () => {
     const index = button.getAttribute('data-index');
     removeBook(index);
     grid.removeChild(card);
   });
-  return button;
-}
-
-function addBookToGrid(book, index) {
-  const grid = document.getElementById('bookGrid');
-  const card = createBookCard(book, index);
-  const button = createRemoveButton(index, card, grid);
 
   grid.appendChild(card);
-  card.appendChild(button);
-}
+};
 
-function removeBook(index) {
+const removeBook = (index) => {
   library.splice(index, 1);
-}
+};
+
+const updateBookList = () => {
+  const grid = document.getElementById('bookGrid');
+  grid.textContent = '';
+  library.forEach((book, index) => {
+    createCard(book, index);
+  });
+};
 
 document.getElementById('form').addEventListener('submit', (event) => {
   event.preventDefault();
   const form = event.currentTarget;
   addBookToLibrary(form);
-  addBookToGrid(library[library.length - 1], library.length - 1);
+  updateBookList();
   form.reset();
 });
 
-addBookToGrid(library[0]);
-addBookToGrid(library[1]);
+updateBookList();
