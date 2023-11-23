07330cc4079c86d1c3515d61fe5d1d73e9562fb8
Luke Ramljak <lukeramljak@proton.me>
2023-11-23 21:07:48 +0800

    small changes

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const library = [];
+const library = [
+  {
+    title: 'Book',
+    author: 'Author',
+    pages: 100,
+    read: 'Read',
+  },
+];
 
 function Book(title, author, pages, read) {
   (this.title = title),
@@ -23,17 +30,22 @@ const createCard = (book, index) => {
 
   card.className = 'card';
   card.setAttribute('data-index', index);
-  card.textContent = `${book.title} by ${book.author}, ${book.pages} pages.`;
+  card.textContent = `${book.title} by ${book.author}, ${book.pages} pages. ${book.read}`;
 
-  const button = document.createElement('button');
-  button.className = 'btn-remove';
-  button.setAttribute('data-index', index);
-  button.textContent = 'Remove';
+  const readButton = document.createElement('button');
+  readButton.className = 'btn-read';
+  readButton.setAttribute('data-index', index);
+  readButton.textContent = 'Toggle';
 
-  card.appendChild(button);
+  const removeButton = document.createElement('button');
+  removeButton.className = 'btn-remove';
+  removeButton.setAttribute('data-index', index);
+  removeButton.textContent = 'Remove';
 
-  button.addEventListener('click', () => {
-    const index = button.getAttribute('data-index');
+  card.appendChild(readButton);
+  card.appendChild(removeButton);
+
+  removeButton.addEventListener('click', (index) => {
     library.splice(index, 1);
     updateBookList();
   });
@@ -44,17 +56,18 @@ const createCard = (book, index) => {
 const updateBookList = () => {
   const grid = document.getElementById('bookGrid');
   const message = document.createElement('p');
+
   grid.textContent = '';
+
   if (library.length === 0) {
     grid.appendChild(message);
     message.textContent = 'No books yet.';
+  } else {
+    library.forEach((book, index) => {
+      createCard(book, index);
+    });
   }
-  library.forEach((book, index) => {
-    message.textContent = '';
-    createCard(book, index);
-  });
 };
-
 document.getElementById('form').addEventListener('submit', (event) => {
   event.preventDefault();
   const form = event.currentTarget;
@@ -62,3 +75,5 @@ document.getElementById('form').addEventListener('submit', (event) => {
   updateBookList();
   form.reset();
 });
+
+updateBookList();
