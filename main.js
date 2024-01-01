4955ca2332f00414552a3623352f3bd3bde75766
lukeramljak <lukeramljak@luke-debian.myguest.virtualbox.org>
2024-01-01 16:24:52 +0800

    move event handling to module

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 import Book from "./book.js";
-import {
-  handleButtonClick,
-  renderBookCards,
-  setInvalidFields,
-} from "./utils.js";
+import { renderBookCards } from "./utils.js";
 
-const library = [];
+export const library = [];
 
 library.push(
   new Book("Lord of the Flies", "William Golding", 224, true),
@@ -13,40 +9,3 @@ library.push(
 );
 
 renderBookCards();
-
-export { library };
-
-document.addEventListener("click", handleButtonClick);
-
-document.getElementById("form").addEventListener("submit", (event) => {
-  event.preventDefault();
-  const modal = document.querySelector("dialog");
-  const form = event.currentTarget;
-  const button = form.querySelector(".btn-add");
-
-  if (!form.checkValidity()) {
-    setInvalidFields();
-  }
-
-  if (form.checkValidity() && button.textContent === "Add Book") {
-    const book = new Book(
-      form.title.value,
-      form.author.value,
-      parseInt(form.pages.value),
-      form.read.value === "read" ? true : false,
-    );
-    book.addToLibrary();
-
-    form.reset();
-    modal.close();
-  } else if (form.checkValidity() && button.textContent === "Update") {
-    const book = library[form.getAttribute("data-editing")];
-    book.title = form.title.value;
-    book.author = form.author.value;
-    book.pages = parseInt(form.pages.value);
-    book.read = form.read.value === "read" ? true : false;
-    form.reset();
-    modal.close();
-    renderBookCards();
-  }
-});
